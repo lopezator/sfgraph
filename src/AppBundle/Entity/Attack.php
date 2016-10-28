a001d5a9c687a2e0e7feb030ffd3d03ce098cccd
David López <david@senddo.es>
2016-10-28 23:25:22 +0200

    Created basic pokemon entities and its relations

---
+++ src/AppBundle/Entity/Attack.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Attack
+ *
+ * @ORM\Table(name="attack")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\AttackRepository")
+ */
+class Attack
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Attack
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
---
+++ src/AppBundle/Entity/Pokemon.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Pokemon
+ *
+ * @ORM\Table(name="pokemon")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PokemonRepository")
+ */
+
+class Pokemon
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Attack")
+     * @ORM\JoinColumn(name="attack_id", referencedColumnName="id")
+     */
+    private $attacks;
+
+    /**
+     * @ORM\OneToOne(targetEntity="Pokemon")
+     * @ORM\JoinColumn(name="pokemon_id", referencedColumnName="id")
+     */
+    private $evolution;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\OneToOne(targetEntity="Type")
+     * @ORM\JoinColumn(name="type_id", referencedColumnName="id")
+     */
+    private $type;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Pokemon
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
